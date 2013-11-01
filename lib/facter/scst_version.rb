87ba0af62f343859f55d33820e0502db26c848de
Derek Harkness <dharknes@merit.edu>
2013-11-01 13:17:27 -0400

    Fixed minor reporting error in scst version fact

--- lib/facter/scst_version.rb
+++ lib/facter/scst_version.rb
@@ -1,13 +1,17 @@
 Facter.add(:scst_version) do
   confine :operatingsystem => %w{RedHat Centos}
   setcode do
-    cmd = "/usr/bin/head -1 /sys/kernel/scst_tgt/version"
-    version = Facter::Util::Resolution.exec(cmd)
+    if File.exists?('/sys/kernel/scst_tgt/version')
+        cmd = "/usr/bin/head -1 /sys/kernel/scst_tgt/version"
+        version = Facter::Util::Resolution.exec(cmd)
 
-    if version.nil? or !version.match(/\d+\.\d+/)
-      nil
+        if version.nil? or !version.match(/\d+\.\d+/)
+            nil
+        else
+            version
+        end
     else
-      version
+        nil
     end
   end
 end
